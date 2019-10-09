50c934f20614a1121f07a4f92fd28903cc62ea41
EEEeelijaHhHHh <elijahs.adjutovs@gmail.com>
2019-10-09 12:11:12 +0300

    Adaptive Main Content && Sidebar && Bets Box

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 (function () {
   const teamsSlider = new Swiper('.teams', {
-    slidesPerView: 10,
-    slidesPerGroup: 5,
+    slidesPerView: 4,
     spaceBetween: 5,
     loop: true,
     navigation: {
@@ -10,8 +9,26 @@
     },
     autoplay: {
       delay: 3000
+    },
+    breakpoints: {
+      576: {
+        slidesPerView: 8,
+        slidesPerGroup: 4
+      },
+      769: {
+        slidesPerView: 4,
+        slidesPerGroup: 2
+      },
+      992: {
+        slidesPerView: 8,
+        slidesPerGroup: 4
+      },
+      1200: {
+        slidesPerView: 10,
+        slidesPerGroup: 5
+      }
     }
-  })
+  });
 }());
 
 (function () {
@@ -23,5 +40,28 @@
     navDropdown.classList.toggle('nav-dropdown_active');
     navBtn.classList.toggle('nav-menu-hamburger_active');
     hoverItems.forEach(item => item.classList.toggle('hover_active'));
-  })
+  });
+}());
+
+(function () {
+  window.addEventListener('resize', () => {
+    adaptive();
+  });
+  adaptive();
+
+  function adaptive() {
+    const width = window.innerWidth;
+    const betsItems = document.querySelectorAll('.bets__item');
+
+    betsItems.forEach(item => {
+      const infoBlock = item.querySelector('.bets__info');
+      const sidebarBlock = item.querySelector('.bets__sidebar');
+      const btn = item.querySelector('.bets__btn');
+      if (width <= 768) {
+        infoBlock.append(btn);
+      } else {
+        sidebarBlock.append(btn);
+      }
+    });
+  }
 }());
